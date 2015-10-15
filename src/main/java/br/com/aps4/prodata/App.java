b35e6c7db2d3b80337b643b6ca43354cec70fbdf
Andre <andrerdm@msn.com>
2015-10-14 21:31:36 -0300

    meu primeiro commit

---
+++ src/main/java/br/com/aps4/prodata/App.java
@@ -0,0 +1,13 @@
+package br.com.aps4.prodata;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
