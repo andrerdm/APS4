78be56ffb468a4e985734227ee45c491426bd013
André <Andr@DESKTOP-4NC9GGS>
2015-11-20 15:35:15 -0200

    criação da pagina de login

--- src/main/java/br/com/apss4/prodata/controller/loginController.java
+++ src/main/java/br/com/apss4/prodata/controller/loginController.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("loginController.do")
+@WebServlet("/loginController")
 public class loginController extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
@@ -27,7 +27,12 @@ public class loginController extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
-		System.out.println("Teste OK");
+		String login = req.getParameter("login");
+		String senha = req.getParameter("senha");
+		
+		System.out.println(login +" - "+ senha);
+		
+		resp.getWriter().print("<h1>Sucesso</h1>");
 	}
 	
 }
